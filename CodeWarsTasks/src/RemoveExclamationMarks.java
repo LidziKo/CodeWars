ca49076f42a73a63d216768d366db85a3d6ff689
LIDZIKO <lidia1473@gmail.com>
2022-03-27 21:39:40 +0300

    27032022 codewars new repo

---
+++ CodeWarsTasks/src/RemoveExclamationMarks.java
@@ -0,0 +1,13 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+public class RemoveExclamationMarks {
+    public static void main(String[] args) {
+        String s = "Hello! world!";
+
+        System.out.println(removeExclamationMarks(s));
+    }
+
+    static String removeExclamationMarks(String s) {
+        return s.replaceAll("!", "");
+    }
+}
